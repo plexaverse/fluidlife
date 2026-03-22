4a957b291a5ab399d940a18840decd109c2e3d5a
asangzz <takekare.in@gmail.com>
2026-03-23 02:04:52 +0530

    dark mode activated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const urbanist = Urbanist({
   variable: "--font-urbanist",
@@ -21,8 +22,18 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${urbanist.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
